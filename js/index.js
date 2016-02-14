9f14ed796225a398e0e2a2bac4036c576ce04a32
wongherlung <wongherlung@gmail.com>
2016-02-14 23:38:24 +0800

    done with languages.

--- js/index.js
+++ js/index.js
@@ -3,10 +3,43 @@
  */
 $(document).ready(function() {
 
+  attachScrollEvents();
+  attachNavbarEvent();
+
+});
+
+function attachScrollEvents() {
+  var animationTime = 500
+
   $('#splash-btn').click(function() {
     $('html, body').animate({
       scrollTop: $("#content-1").offset().top
-    }, 500);
+    }, animationTime);
   });
 
-});
+  $('#content-1-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-2").offset().top
+    }, animationTime);
+  });
+
+  $('#content-2-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-3").offset().top
+    }, animationTime);
+  });
+}
+
+function attachNavbarEvent() {
+  var navbarThreshold = $('#content-1').offset().top;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= navbarThreshold) {
+      $('.navbar').hide();
+    } else {
+      $('.navbar').show();
+      $('.navbar').css({
+        'opacity': 1 - $(this).scrollTop()/navbarThreshold
+      });
+    }
+  });
+}
