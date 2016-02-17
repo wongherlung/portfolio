cb7f5d00efef43f95407f5d79332075024dc45ac
wongherlung <wongherlung@gmail.com>
2016-02-17 16:14:33 +0800

    validation is done.

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 
   attachScrollEvents();
   attachNavbarEvent();
+  attachValidation();
 
 });
 
@@ -28,6 +29,30 @@ function attachScrollEvents() {
       scrollTop: $("#content-3").offset().top
     }, animationTime);
   });
+
+  $('#viddsee-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-4").offset().top
+    }, animationTime);
+  });
+
+  $('#xfers-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-4").offset().top
+    }, animationTime);
+  });
+
+  $('#cvwo-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-4").offset().top
+    }, animationTime);
+  });
+
+  $('#content-4-btn').click(function() {
+    $('html, body').animate({
+      scrollTop: $("#content-3").offset().top
+    }, animationTime);
+  });
 }
 
 function attachNavbarEvent() {
@@ -42,4 +67,60 @@ function attachNavbarEvent() {
       });
     }
   });
+}
+
+function attachValidation() {
+  $("#submit-message").click(function() {
+    var email = $("#email").val();
+    var message = $("#message").val();
+    var emailErrorMessage = '';
+    var messageErrorMessage = '';
+    $('.email-form-group').removeClass('has-error');
+    $('.message-form-group').removeClass('has-error');
+    $('#email-help-block').text('');
+    $('#message-help-block').text('');
+
+    if (!email) {
+      $('.email-form-group').addClass('has-error');
+      emailErrorMessage += 'Email field is empty. ';
+    }
+
+    if (!message) {
+      $('.message-form-group').addClass('has-error');
+      messageErrorMessage += 'Message field is empty. ';
+    }
+
+    if (email && !validateEmail(email)) {
+      $('.email-form-group').addClass('has-error');
+      emailErrorMessage += 'Email is not a valid email format. ';
+    }
+
+    if (emailErrorMessage) {
+      $('#email-help-block').text(emailErrorMessage);
+    }
+
+    if (messageErrorMessage) {
+      $('#message-help-block').text(messageErrorMessage);
+    }
+
+    if (emailErrorMessage || messageErrorMessage) {
+      return;
+    }
+
+    alert('Thank you for contacting me. I\'ll get in touch with you soon!');
+    $("#email").val('');
+    $("#message").val('');
+    var emailErrorMessage = '';
+    var messageErrorMessage = '';
+    $('.email-form-group').removeClass('has-error');
+    $('.message-form-group').removeClass('has-error');
+    $('#email-help-block').text('');
+    $('#message-help-block').text('');
+
+  });
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
